310a2bf84af6c54e3c56322d25b99c74f283c351
Андрей <vsscomp@mail.ru>
2023-12-04 13:57:50 +0300

    change webdriver

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.BeforeAll;
 public class BaseTest {
     @BeforeAll
     public static void beforeAll() {
-        System.setProperty("selenide.browser", "firefox");
+        //System.setProperty("selenide.browser", "firefox");
+        System.setProperty("webdriver.chrome.driver", "D:\\!документы Андрей\\Desktop\\AutoTesting\\M2.ru_AutoTesting\\chromedriver.exe");
         Configuration.browserSize = "1920x1080";
         Selenide.open("https://m2.ru/");
     }
